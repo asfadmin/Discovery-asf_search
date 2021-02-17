8440dba01e25bfeec25fbf98e95c03766a9abec3
Gregory Short <glshort@alaska.edu>
2021-02-17 15:00:41 -0600

    Update setup.py with BSD-3 license

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3",
+        "License :: OSI Approved :: BSD-3",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
