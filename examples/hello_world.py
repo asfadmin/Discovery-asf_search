0fa0ba57c8a84eb8b78a6569579802f14c97a130
glshort <glshort@alaska.edu>
2021-02-17 21:29:19 -0600

    Clean up hello_world.py example a little.

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,9 +1,8 @@
 import json
 import asf_search as asf
 
-print(asf.DATASET.AVNIR)
-print(asf.BEAMMODE.IW)
-print(asf.POLARIZATION.HH_HV)
-print(asf.PLATFORM.SENTINEL1)
-print(f'https://{asf.INTERNAL.HOST}{asf.INTERNAL.HEALTH_PATH}')
-print(json.dumps(asf.health(), indent=2))
+print(f'asf.DATASET.AVNIR: {asf.DATASET.AVNIR}')
+print(f'asf.BEAMMODE.IW: {asf.BEAMMODE.IW}')
+print(f'asf.POLARIZATION.HH_HV: {asf.POLARIZATION.HH_HV}')
+print(f'asf.PLATFORM.SENTINEL1: {asf.PLATFORM.SENTINEL1}')
+print(f'Health check: {json.dumps(asf.health(), indent=2)}')
