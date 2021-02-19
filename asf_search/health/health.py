815fa6dcca139f6cb640d55959b8c0707909aa7b
glshort <glshort@alaska.edu>
2021-02-19 12:26:30 -0600

    Add return type hint on health function

--- asf_search/health/health.py
+++ asf_search/health/health.py
@@ -2,5 +2,5 @@ import requests
 import json
 import asf_search.constants
 
-def health():
+def health() -> dict:
     return json.loads(requests.get(f'https://{asf_search.INTERNAL.HOST}{asf_search.INTERNAL.HEALTH_PATH}').text)
