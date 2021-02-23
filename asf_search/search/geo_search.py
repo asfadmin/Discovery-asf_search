e7750f6653124057987d629c9f608f111e26e09d
Gregory Short <glshort@alaska.edu>
2021-02-22 21:16:22 -0600

    Initial support for geosearch.
    Camelcase search function args

--- asf_search/search/geo_search.py
+++ asf_search/search/geo_search.py
@@ -1,10 +1,8 @@
-import requests
-import datetime
-import json
 from typing import Union, Iterable
-import asf_search.constants
+import datetime
+import asf_search.search
 
-def search(
+def geo_search(
         absoluteOrbit: Iterable[Union[int, range]] = None,
         asfFrame: Iterable[Union[int, range]] = None,
         beamMode: Iterable[str] = None,
@@ -12,8 +10,6 @@ def search(
         end: datetime = None,
         flightDirection: Iterable[str] = None,
         frame: Iterable[Union[int, range]] = None,
-        granule_list: Iterable[str] = None,
-        groupID: Iterable[str] = None,
         instrument: Iterable[str] = None,
         intersectsWith: str = None,
         lookDirection: Iterable[str] = None,
@@ -22,13 +18,12 @@ def search(
         polarization: Iterable[str] = None,
         processingDate: datetime = None,
         processingLevel: Iterable[str] = None,
-        product_list: Iterable[str] = None,
         relativeOrbit: Iterable[Union[int, range]] = None,
         start: datetime = None,
-        host: str = None
+        host: str = asf_search.INTERNAL.HOST
 ) -> dict:
     """
-    Performs a generic search using the ASF SearchAPI
+    Performs a geographic search using the ASF SearchAPI
 
     :param absoluteOrbit: For ALOS, ERS-1, ERS-2, JERS-1, and RADARSAT-1, Sentinel-1A, Sentinel-1B this value corresponds to the orbit count within the orbit cycle. For UAVSAR it is the Flight ID.
     :param asfFrame: This is primarily an ASF / JAXA frame reference. However, some platforms use other conventions. See ‘frame’ for ESA-centric frame searches.
@@ -37,8 +32,6 @@ def search(
     :param end: End date of data acquisition. Supports timestamps as well as natural language such as "3 weeks ago"
     :param flightDirection: Satellite orbit direction during data acquisition
     :param frame: ESA-referenced frames are offered to give users a universal framing convention. Each ESA frame has a corresponding ASF frame assigned. See also: asfframe
-    :param granule_list: List of specific granules. Search results may include several products per granule name.
-    :param groupID: Identifier used to find products considered to be of the same scene but having different granule names
     :param instrument: The instrument used to acquire the data. See also: platform
     :param intersectsWith: Search by polygon, linestring, or point defined in 2D Well-Known Text (WKT)
     :param lookDirection: Left or right look direction during data acquisition
@@ -47,7 +40,6 @@ def search(
     :param polarization: A property of SAR electromagnetic waves that can be used to extract meaningful information about surface properties of the earth.
     :param processingDate: Used to find data that has been processed at ASF since a given time and date
     :param processingLevel: Level to which the data has been processed
-    :param product_list: List of specific products. Guaranteed to be at most one product per product name.
     :param relativeOrbit: Path or track of satellite during data acquisition. For UAVSAR it is the Line ID.
     :param start: Start date of data acquisition. Supports timestamps as well as natural language such as "3 weeks ago"
     :param host: SearchAPI host, defaults to Production SearchAPI. This option is intended for dev/test purposes.
@@ -57,8 +49,5 @@ def search(
 
     kwargs = locals()
     data = dict((k,v) for k,v in kwargs.items() if v is not None and v != '')
-    host = data.pop('host', asf_search.INTERNAL.HOST)
 
-    data['output'] = 'geojson'
-    response = requests.post(f'https://{host}{asf_search.INTERNAL.SEARCH_PATH}', data=data)
-    return json.loads(response.text)
+    return asf_search.search(**data)
--- examples/hello_world.py
+++ examples/hello_world.py
@@ -6,6 +6,7 @@ import json
 import asf_search as asf
 
 print('='*80)
+print('Constants')
 print(f'asf.DATASET.AVNIR: {asf.DATASET.SENTINEL1}')
 print(f'asf.BEAMMODE.IW: {asf.BEAMMODE.IW}')
 print(f'asf.POLARIZATION.HH_HV: {asf.POLARIZATION.HH_HV}')
@@ -15,7 +16,7 @@ print('='*80)
 print(f'Health check: {json.dumps(asf.health(), indent=2)}')
 
 print('='*80)
-results = asf.search(platform='S1', maxresults=2)
+results = asf.search(platform='S1', maxResults=2)
 print(f'Basic search check: {json.dumps(results, indent=2)}')
 
 print('='*80)
@@ -25,3 +26,8 @@ print(f'Granule search check: {json.dumps(results, indent=2)}')
 print('='*80)
 results = asf.product_search(['ALAV2A279102730','ALAV2A279133150'])
 print(f'Product search check: {json.dumps(results, indent=2)}')
+
+print('='*80)
+wkt = 'POLYGON((-135.7843 58.2625,-136.6521 58.1589,-135.8928 56.9884,-134.6724 56.1857,-134.9571 58.0335,-135.7843 58.2625))'
+results = asf.geo_search(platform=asf.PLATFORM.SENTINEL1, intersectsWith=wkt, maxResults=2)
+print(f'Geographic search check: {json.dumps(results, indent=2)}')
