4ec45e8a85cd626ea92f83937df9f8f04e0f7f4f
Gregory Short <glshort@alaska.edu>
2021-03-22 16:57:41 -0500

    Unpin requests & numpy versions

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup
 exec(open('asf_search/version.py').read())
 
 requirements = [
-        "requests==2.25.1",
-        "numpy==1.20.1"
+        "requests",
+        "numpy"
     ]
 
 with open("README.md", "r") as readme_file:
