5f6fa1da17834f4104ab94731f18c83b34549800
glshort <glshort@alaska.edu>
2021-04-09 09:57:27 -0500

    Add ASFSearchResults(dict) class, in order to provide some extra functionality like download_all

---
+++ asf_search/search/results.py
@@ -0,0 +1,11 @@
+from .products import ASFProduct
+
+class ASFSearchResults(dict):
+    def __init__(self, results: dict):
+        super(ASFSearchResults, self).__init__(results)
+        for index, product in enumerate(self['features']):
+            self['features'][index] = ASFProduct(self['features'][index])
+
+    def download_all(self, dir: str):
+        for product in self['features']:
+            product.download(dir=dir)
