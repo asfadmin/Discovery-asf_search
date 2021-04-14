b6ba0deccc3ae5cdd35449ad0da3684bb72c5744
glshort <glshort@alaska.edu>
2021-04-14 09:18:13 -0500

    Remove DATASET constants, as these are not actually searchable (use platform+instrument to explicitly identify a dataset)

--- asf_search/constants/__init__.py
+++ asf_search/constants/__init__.py
@@ -5,7 +5,6 @@ from .FLIGHT_DIRECTION import *
 from .PLATFORM import *
 from .POLARIZATION import *
 from .PRODUCT_TYPE import *
-from .DATASET import *
 from .INTERNAL import *
 
 #TODO: add INSTRUMENT constants
