0b78b8a7897a3e48bfe1c5aca17776def9b8b9cd
glshort <glshort@alaska.edu>
2021-04-14 09:10:18 -0500

    Add download support, currently only support authentication via token

--- asf_search/search/results.py
+++ asf_search/search/results.py
@@ -6,6 +6,15 @@ class ASFSearchResults(dict):
         for index, product in enumerate(self['features']):
             self['features'][index] = ASFProduct(self['features'][index])
 
-    def download_all(self, dir: str):
+    def download(self, dir: str, token: str = None) -> None:
+        """
+        Iterates over each ASFProduct and downloads them to the specified directory.
+
+        :param dir: The directory into which the products should be downloaded.
+        :param token: EDL authentication token for authenticated downloads, see https://urs.earthdata.nasa.gov/user_tokens
+
+        :return: None
+        """
+
         for product in self['features']:
-            product.download(dir=dir)
+            product.download(dir=dir, token=token)
