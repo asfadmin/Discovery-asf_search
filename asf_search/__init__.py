d4a3a35770fff56f5c00831e68e89b91e7430a5c
Cameron Showalter <cjshowalter@alaska.edu>
2021-10-07 12:36:16 -0800

    Switching from default 0.0.0, to throwing an error

--- asf_search/__init__.py
+++ asf_search/__init__.py
@@ -3,13 +3,13 @@ from importlib_metadata import PackageNotFoundError, version
 
 try:
     __version__ = version(__name__)
-except PackageNotFoundError:
+except PackageNotFoundError as e:
     print('package is not installed!\n'
           'Install in editable/develop mode via (from the top of this repo):\n'
           '   python -m pip install -e .\n'
           'Or, to just get the version number use:\n'
           '   python setup.py --version')
-    __version__ = "0.0.0"
+    raise PackageNotFoundError("Install with 'python -m pip install -e .' to use") from e
 
 from .ASFSession import ASFSession
 from .ASFProduct import ASFProduct
