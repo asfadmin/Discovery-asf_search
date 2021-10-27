5ed9087676ec39bd377bf1b2fec8d2463b71234b
glshort <glshort@alaska.edu>
2021-10-26 19:40:30 -0500

    Expose stack_from_product, get_stack_opts

--- asf_search/search/__init__.py
+++ asf_search/search/__init__.py
@@ -2,4 +2,4 @@ from .search import search
 from .granule_search import granule_search
 from .product_search import product_search
 from .geo_search import geo_search
-from .baseline_search import stack_from_id
+from .baseline_search import stack_from_id, stack_from_product, get_stack_opts
