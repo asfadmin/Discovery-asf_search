1152f091760d8140f5652edf5244cc6fcc786da3
glshort <glshort@alaska.edu>
2021-10-26 19:52:15 -0500

    Remove unused import

--- asf_search/search/baseline_search.py
+++ asf_search/search/baseline_search.py
@@ -7,7 +7,7 @@ from asf_search.ASFSearchOptions import ASFSearchOptions
 from asf_search.ASFSearchResults import ASFSearchResults
 from asf_search.ASFProduct import ASFProduct
 from asf_search.ASFSession import ASFSession
-from asf_search.constants import INTERNAL, PLATFORM
+from asf_search.constants import PLATFORM
 from asf_search.exceptions import ASFSearchError, ASFBaselineError
 
 
