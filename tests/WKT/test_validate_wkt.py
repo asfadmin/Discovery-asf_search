4b6ff09b9ed906e4893f66efdea937610edf059d
kim <jdfairbanks@alaska.edu>
2022-03-24 09:57:11 -0800

    adds more comments to wkt_validation, removes some redundant tests

--- tests/WKT/test_validate_wkt.py
+++ tests/WKT/test_validate_wkt.py
@@ -18,21 +18,9 @@ from asf_search.WKT.validate_wkt import (
 from asf_search.exceptions import ASFWKTError
 
 
-def run_test_validate_wkt_invalid_type_error(wkt: str):
-    with pytest.raises(ASFWKTError):
-        validate_wkt(wkt)
-
-
 def run_test_validate_wkt_invalid_wkt_error(wkt: str):
     with pytest.raises(ASFWKTError):
         validate_wkt(wkt)
-
-
-def run_test_validate_wkt_winding_order(wkt: str):
-    original_order = loads(wkt)
-    valdiated = validate_wkt(original_order.wkt)
-
-    assert valdiated != original_order
 
 
 def run_test_valdiate_wkt_valid_wkt(wkt: str, validated_wkt: str):
--- tests/pytest-managers.py
+++ tests/pytest-managers.py
@@ -14,7 +14,7 @@ import os
 import pathlib
 import yaml
 
-from tests.WKT.test_validate_wkt import run_test_search_wkt_prep, run_test_valdiate_wkt_get_shape_coords, run_test_validate_wkt_clamp_geometry, run_test_valdiate_wkt_valid_wkt, run_test_validate_wkt_convex_hull, run_test_validate_wkt_counter_clockwise_reorientation, run_test_validate_wkt_invalid_type_error, run_test_validate_wkt_invalid_wkt_error, run_test_validate_wkt_merge_overlapping_geometry, run_test_validate_wkt_winding_order
+from tests.WKT.test_validate_wkt import run_test_search_wkt_prep, run_test_valdiate_wkt_get_shape_coords, run_test_validate_wkt_clamp_geometry, run_test_valdiate_wkt_valid_wkt, run_test_validate_wkt_convex_hull, run_test_validate_wkt_counter_clockwise_reorientation, run_test_validate_wkt_invalid_wkt_error, run_test_validate_wkt_merge_overlapping_geometry
 
 # asf_search.ASFProduct Tests
 def test_ASFProduct(**args) -> None:
@@ -169,29 +169,28 @@ def test_ASFSearch_Search_Error(**args) -> None:
         with raises(ASFSearch5xxError):
             run_test_search_http_error(parameters, error_code, report)
 
-def test_wkt_validation_Type_Error(**args) -> None:
-    test_info = args["test_info"]
-    wkt = get_resource(test_info['wkt'])
-    
-    run_test_validate_wkt_invalid_type_error(wkt)
-    
 def test_wkt_validation_Invalid_WKT_Error(**args) -> None:
+    """
+    Test asf_search.wkt errors,
+    asserting wkt validation errors are raised
+    """
     test_info = args["test_info"]
     wkt = get_resource(test_info['wkt'])
     run_test_validate_wkt_invalid_wkt_error(wkt)
 
-def test_wkt_validation_WKT_Winding_Order(**args) -> None:
-    test_info = args["test_info"]
-    wkt = get_resource(test_info['wkt'])
-    run_test_validate_wkt_winding_order(wkt)
-
 def test_wkt_validation_WKT_Valid(**args) -> None:
+    """
+    Test asf_search.validate_wkt, asserting expected wkts are returned
+    """
     test_info = args["test_info"]
     wkt = get_resource(test_info['wkt'])
     validated_wkt = get_resource(test_info['validated-wkt'])
     run_test_valdiate_wkt_valid_wkt(wkt, validated_wkt)
 
 def test_wkt_validation_WKT_clamp_geometry(**args) -> None:
+    """
+    Test asf_search.validate_wkt._get_clamped_geometry, asserting the amount of clamped and wrapped coordinates
+    """
     test_info = args["test_info"]
     wkt = get_resource(test_info['wkt'])
     clamped_wkt = get_resource(test_info['clamped-wkt'])
@@ -200,22 +199,50 @@ def test_wkt_validation_WKT_clamp_geometry(**args) -> None:
     run_test_validate_wkt_clamp_geometry(wkt, clamped_wkt, clamped_count, wrapped_count)
 
 def test_wkt_validation_convex_hull(**args) -> None:
+    """
+    Test asf_search.validate_wkt._get_convex_hull, asserting convex hulls producted are expected
+    """
     test_info = args["test_info"]
     wkt = get_resource(test_info['wkt'])
     convex_wkt = get_resource(test_info['convex-wkt'])
     run_test_validate_wkt_convex_hull(wkt, convex_wkt)
 
 def test_wkt_validation_merge_overlapping_geometry(**args) -> None:
+    """
+    Test asf_search.validate_wkt._merge_overlapping_geometry, asserting expected shapes are merged
+    """
     test_info = args["test_info"]
     wkt = get_resource(test_info['wkt'])
     merged_wkt = get_resource(test_info['merged-wkt'])
     run_test_validate_wkt_merge_overlapping_geometry(wkt, merged_wkt)
 
 def test_wkt_validation_counter_clockwise_reorientation(**args) -> None:
+    """
+    Test asf_search.validate_wkt._counter_clockwise_reorientation reverses polygon orientation if polygon is wound clockwise,
+    and maintains counter-clockwise winding when polygon orientation is correct
+    """
     test_info = args["test_info"]
     wkt = get_resource(test_info['wkt'])
     cc_wkt = get_resource(test_info['cc-wkt'])
     run_test_validate_wkt_counter_clockwise_reorientation(wkt, cc_wkt)
+
+def test_valdiate_wkt_get_shape_coords(**args) -> None:
+    """
+    Test asf_search.validate_wkt._get_shape_coords asserting all coordinates are returned and expected
+    """
+    test_info = args["test_info"]
+    wkt = get_resource(test_info['wkt'])
+    coords = get_resource(test_info['coordinates'])
+    run_test_valdiate_wkt_get_shape_coords(wkt, coords)
+
+def test_search_wkt_prep(**args) -> None:
+    """
+    Test asf_search.validate_wkt.wkt_prep, asserting returned shape is correct geometric type and expected shape
+    """
+    test_info = args["test_info"]
+    wkt = get_resource(test_info['wkt'])
+    
+    run_test_search_wkt_prep(wkt)
 
 def test_get_platform_collection_names(**args) -> None:
     test_info = args["test_info"]
@@ -223,18 +250,6 @@ def test_get_platform_collection_names(**args) -> None:
     campaigns: List[str] = get_resource(test_info["campaigns"])
     
     run_test_get_project_names(cmr_ummjson, campaigns)
-
-def test_valdiate_wkt_get_shape_coords(**args) -> None:
-    test_info = args["test_info"]
-    wkt = get_resource(test_info['wkt'])
-    coords = get_resource(test_info['coordinates'])
-    run_test_valdiate_wkt_get_shape_coords(wkt, coords)
-
-def test_search_wkt_prep(**args) -> None:
-    test_info = args["test_info"]
-    wkt = get_resource(test_info['wkt'])
-    
-    run_test_search_wkt_prep(wkt)
 
 def get_resource(yml_file):
     
