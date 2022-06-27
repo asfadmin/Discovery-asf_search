023eed65e3d200e804dc3a940d10d2e0587aedbe
kim <jdfairbanks@alaska.edu>
2022-06-27 15:23:50 -0800

    changes count() to search_count(), updates where needed

--- asf_search/search/__init__.py
+++ asf_search/search/__init__.py
@@ -4,4 +4,4 @@ from .product_search import product_search
 from .geo_search import geo_search
 from .baseline_search import stack_from_id, get_stack_opts
 from .campaigns import campaigns
-from .count import count
+from .search_count import search_count
--- asf_search/search/search_count.py
+++ asf_search/search/search_count.py
@@ -8,7 +8,7 @@ from asf_search.CMR import translate_opts
 from asf_search.search.search import get_page, preprocess_opts
 from asf_search import INTERNAL
 
-def count(        
+def search_count(        
         absoluteOrbit: Union[int, Tuple[int, int], Iterable[Union[int, Tuple[int, int]]]] = None,
         asfFrame: Union[int, Tuple[int, int], Iterable[Union[int, Tuple[int, int]]]] = None,
         beamMode: Union[str, Iterable[str]] = None,
