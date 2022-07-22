674c015d8bfdab690848446e7fe4707b4315d01c
kim <jdfairbanks@alaska.edu>
2022-07-22 10:00:01 -0800

    adds distance threshold to search result aoi test cases

--- tests/ASFSearchResults/test_ASFSearchResults.py
+++ tests/ASFSearchResults/test_ASFSearchResults.py
@@ -2,10 +2,13 @@ from json import load
 import asf_search as asf
 from shapely.wkt import loads
 from shapely.geometry import shape
+from shapely.ops import unary_union
 from asf_search.constants import PLATFORM
 def run_test_ASFSearchResults_intersection(wkt: str):
     aoi = loads(wkt)
-
+    validated = asf.validate_wkt(aoi)
+    aoi = unary_union([aoi, validated])
+    
     # exclude SMAP products
     platforms = [PLATFORM.AIRSAR, 
                  PLATFORM.ALOS, 
@@ -22,4 +25,4 @@ def run_test_ASFSearchResults_intersection(wkt: str):
     
         for product in results:
             product_geom = shape(product.geometry)
-            assert product_geom.overlaps(aoi) or product_geom.intersects(aoi) or product_geom.contains(aoi) or product_geom.touches(aoi) or product_geom.within(aoi) or aoi.within(product_geom), f"OVERLAP FAIL: {product.properties['sceneName']}, {product.geometry}"
+            assert product_geom.overlaps(aoi) or product_geom.intersects(aoi) or product_geom.contains(aoi) or product_geom.touches(aoi) or product_geom.within(aoi) or aoi.within(product_geom) or aoi.distance(product_geom) <= 0.0015, f"OVERLAP FAIL: {product.properties['sceneName']}, {product.geometry}"
