493000ec42843335cff35f382378024b1796b18c
kim <jdfairbanks@alaska.edu>
2022-07-25 14:56:24 -0800

    adds test at higher lat, and other test over australia

--- tests/ASFSearchResults/test_ASFSearchResults.py
+++ tests/ASFSearchResults/test_ASFSearchResults.py
@@ -1,12 +1,13 @@
 from json import load
 import asf_search as asf
 from shapely.wkt import loads
+from shapely.ops import transform
 from shapely.geometry import shape
 from shapely.geometry.base import BaseGeometry
 from asf_search.constants import PLATFORM
 def run_test_ASFSearchResults_intersection(wkt: str):
     aoi = asf.validate_wkt(wkt)
-    unchanged_aoi = loads(wkt)
+    unchanged_aoi = loads(wkt) # sometimes geometries don't come back with wrapping in mind
 
     # exclude SMAP products
     platforms = [
@@ -22,7 +23,7 @@ def run_test_ASFSearchResults_intersection(wkt: str):
                  ]   
     
     def overlap_check(s1: BaseGeometry, s2: BaseGeometry):
-        return s1.overlaps(s2) or s1.touches(s2) or s2.distance(s1) <= 0.0015
+        return s1.overlaps(s2) or s1.touches(s2) or s2.distance(s1) <= 0.005
 
     for platform in platforms:
         results = asf.geo_search(intersectsWith=wkt, platform=platform, maxResults=250)
@@ -30,4 +31,10 @@ def run_test_ASFSearchResults_intersection(wkt: str):
         for product in results:
             if shape(product.geometry).is_valid:
                 product_geom = asf.validate_wkt(shape(product.geometry))
-                assert overlap_check(product_geom, aoi) or overlap_check(product_geom, unchanged_aoi), f"OVERLAP FAIL: {product.properties['sceneName']}, {product.geometry} \nproduct: {product_geom.wkt} \naoi: {aoi.wkt}"
+                original_shape = unchanged_aoi
+
+                # Shapes crossing antimeridian might have coordinates starting from other side
+                if unchanged_aoi.bounds[2] < 0 and product_geom.bounds[0] > 0:
+                    original_shape = transform(lambda x, y, z=None: tuple([x + 360, y]), unchanged_aoi)
+
+                assert overlap_check(product_geom, aoi) or overlap_check(product_geom, original_shape), f"OVERLAP FAIL: {product.properties['sceneName']}, {product.geometry} \nproduct: {product_geom.wkt} \naoi: {aoi.wkt}"
