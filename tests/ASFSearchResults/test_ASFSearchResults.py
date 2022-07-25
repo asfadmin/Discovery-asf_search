2db2f2197d08e36c43fdadeeac823d7a78ed5583
kim <jdfairbanks@alaska.edu>
2022-07-25 11:47:01 -0800

    removes unused imports from test_ASFSearchResults.py

--- tests/ASFSearchResults/test_ASFSearchResults.py
+++ tests/ASFSearchResults/test_ASFSearchResults.py
@@ -1,15 +1,13 @@
 from json import load
-from typing import Tuple
 import asf_search as asf
 from shapely.wkt import loads
-from shapely.geometry import shape, GeometryCollection
+from shapely.geometry import shape
 from shapely.geometry.base import BaseGeometry
-from shapely.ops import unary_union, transform
 from asf_search.constants import PLATFORM
 def run_test_ASFSearchResults_intersection(wkt: str):
     aoi = asf.validate_wkt(wkt)
     unchanged_aoi = loads(wkt)
-    # aoi = GeometryCollection([aoi, loads(wkt)])
+
     # exclude SMAP products
     platforms = [
                  PLATFORM.AIRSAR, 
