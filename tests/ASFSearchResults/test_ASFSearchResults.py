e596bf9255dbe605bcc7197c728050e1677ee72c
kim <jdfairbanks@alaska.edu>
2022-07-25 10:33:55 -0800

    modifies test_ASFSEarchResults.py

--- tests/ASFSearchResults/test_ASFSearchResults.py
+++ tests/ASFSearchResults/test_ASFSearchResults.py
@@ -1,16 +1,18 @@
 from json import load
+from typing import Tuple
 import asf_search as asf
 from shapely.wkt import loads
-from shapely.geometry import shape
-from shapely.ops import unary_union
+from shapely.geometry import shape, GeometryCollection
+from shapely.geometry.base import BaseGeometry
+from shapely.ops import unary_union, transform
 from asf_search.constants import PLATFORM
 def run_test_ASFSearchResults_intersection(wkt: str):
-    aoi = loads(wkt)
-    validated = asf.validate_wkt(aoi)
-    aoi = unary_union([aoi, validated])
-    
+    aoi = asf.validate_wkt(wkt)
+    unchanged_aoi = loads(wkt)
+    # aoi = GeometryCollection([aoi, loads(wkt)])
     # exclude SMAP products
-    platforms = [PLATFORM.AIRSAR, 
+    platforms = [
+                 PLATFORM.AIRSAR, 
                  PLATFORM.ALOS, 
                  PLATFORM.ERS, 
                  PLATFORM.JERS, 
@@ -18,11 +20,16 @@ def run_test_ASFSearchResults_intersection(wkt: str):
                  PLATFORM.SEASAT, 
                  PLATFORM.SENTINEL1, 
                  PLATFORM.SIRC, 
-                 PLATFORM.UAVSAR]   
+                 PLATFORM.UAVSAR
+                 ]   
     
+    def overlap_check(s1: BaseGeometry, s2: BaseGeometry):
+        return s1.overlaps(s2) or s1.touches(s2) or s2.distance(s1) <= 0.0015
+
     for platform in platforms:
         results = asf.geo_search(intersectsWith=wkt, platform=platform, maxResults=250)
     
         for product in results:
-            product_geom = shape(product.geometry)
-            assert product_geom.overlaps(aoi) or product_geom.intersects(aoi) or product_geom.contains(aoi) or product_geom.touches(aoi) or product_geom.within(aoi) or aoi.within(product_geom) or aoi.distance(product_geom) <= 0.0015, f"OVERLAP FAIL: {product.properties['sceneName']}, {product.geometry}"
+            if shape(product.geometry).is_valid:
+                product_geom = asf.validate_wkt(shape(product.geometry))
+                assert overlap_check(product_geom, aoi) or overlap_check(product_geom, unchanged_aoi), f"OVERLAP FAIL: {product.properties['sceneName']}, {product.geometry} \nproduct: {product_geom.wkt} \naoi: {aoi.wkt}"
