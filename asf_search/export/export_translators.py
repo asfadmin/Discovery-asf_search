2838dd72ef23352792ba32ed62c62ee7e6670e5e
kim <jdfairbanks@alaska.edu>
2022-08-19 14:08:58 -0800

    adds some clarifying comments to export_translators.py

--- asf_search/export/export_translators.py
+++ asf_search/export/export_translators.py
@@ -19,28 +19,34 @@ def output_translators():
     }
 
 def results_to_format(get_additional_fields: FunctionType, to_export_format: FunctionType):
-    return lambda results: to_export_format(get_properties_list(results, get_additional_fields))
+    return lambda results: to_export_format(ASFSearchResults_to_properties_list(results, get_additional_fields))
 
-def get_properties_list(results: ASFSearchResults, get_additional_fields):
+# ASFProduct.properties don't have every property required of certain output formats, 
+# This grabs the missing properties from ASFProduct.umm required by the given format
+def ASFSearchResults_to_properties_list(results: ASFSearchResults, get_additional_fields: FunctionType):
     property_list = []
+    
     
     for product in results:
         additional_fields = get_additional_fields(product)
         properties = {**product.properties, **additional_fields}
         property_list.append(properties)
     
+    # Format dates to match format used by SearchAPI output formats
     for product in property_list:
+        # S1 date properties are formatted differently from other platforms
         is_S1 = product['platform'].upper() in ['SENTINEL-1', 'SENTINEL-1B', 'SENTINEL-1A']
         for key, data in product.items():
             if 'date' in key.lower() or 'time' in key.lower():
                 if is_S1:
                     time = datetime.strptime(data[:-1], '%Y-%m-%dT%H:%M:%S.%f')
+                    product[key] = time.strftime('%Y-%m-%dT%H:%M:%S.%f')
                 else:
+                    # Remove trailing zeroes from miliseconds, add Z
                     if len(data.split('.')) == 2:
                         d = len(data.split('.')[0])
                         data = data[:d] + 'Z'
                     time = datetime.strptime(data, '%Y-%m-%dT%H:%M:%SZ')
+                    product[key] = time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-                product[key] = time.strftime('%Y-%m-%dT%H:%M:%S.%f')if  is_S1 else time.strftime('%Y-%m-%dT%H:%M:%SZ')
-    
     return property_list
