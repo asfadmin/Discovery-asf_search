122b33e3e98f7b225bc9f59707de8389e2d24a60
Kim <33294735+SpicyGarlicAlbacoreRoll@users.noreply.github.com>
2022-09-01 08:00:51 -0800

    Revert "ASFSearchResults overlap tests"

--- asf_search/CMR/translate.py
+++ asf_search/CMR/translate.py
@@ -30,13 +30,9 @@ def translate_opts(opts: ASFSearchOptions) -> list:
         # instead of the wkt for better responses from CMR 
         # This will provide better results with AOI's near poles
         if should_use_bbox(shape):
-            bounds = shape.boundary.bounds
-            if bounds[0] > 180 or bounds[2] > 180:
-                bounds = [(x + 180) % 360 - 180 if idx % 2 == 0 and abs(x) > 180 else x for idx, x in enumerate(bounds)] 
-
-            bottom_left = [str(coord) for coord in bounds[:2]]
-            top_right = [str(coord) for coord in bounds[2:]]
-
+            boundary = shape.boundary
+            bottom_left = [str(coord) for coord in boundary.bounds[:2]]
+            top_right = [str(coord) for coord in boundary.bounds[2:]]
             bbox = ','.join([*bottom_left, *top_right])
             dict_opts['bbox'] = bbox
         else:
