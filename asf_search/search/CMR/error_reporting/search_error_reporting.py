19a9bbda1821ed71b526b1add5d99ba074f79aeb
kim <jdfairbanks@alaska.edu>
2022-09-23 15:37:00 -0800

    Adds REPORT_ERRORS var to module scope, updates changelog, examples

--- asf_search/search/CMR/error_reporting/search_error_reporting.py
+++ asf_search/search/CMR/error_reporting/search_error_reporting.py
@@ -4,7 +4,15 @@ import requests
 import logging
 
 def report_search_error(search_options: ASFSearchOptions, message: str):
-    """Reports CMR concept-id errorsReports"""
+    """Reports CMR Errors automatically to ASF"""
+
+    from asf_search import REPORT_ERRORS
+
+    if not REPORT_ERRORS:
+        logging.warning("Automatic search error reporting is turned off, search errors will NOT be reported to ASF.\
+            \nTo enable automatic error reporting, set asf_search.REPORT_ERRORS to True\
+            \nIf you have any questions email uso@asf.alaska.edu")
+        return
 
     end_point_url = "https://search-error-report.asf.alaska.edu"
     
@@ -13,14 +21,15 @@ def report_search_error(search_options: ASFSearchOptions, message: str):
     message=f"Error Message: {str(message)}\nUser Agent: {user_agent} \
     \nSearch Options: {{\n{search_options_list}\n}}"
 
-    r = requests.post(end_point_url, data={'Message': "This error message and info was automatically generated:\n\n" + message})
+    response = requests.post(end_point_url, data={'Message': "This error message and info was automatically generated:\n\n" + message})
 
     try:
-        r.raise_for_status()
+        response.raise_for_status()
     except requests.exceptions.HTTPError as e:
-        logging.error("asf-search failed to automatically report an error, if you have any questions email uso@asf.alaska.edu")
+        logging.error(f"asf-search failed to automatically report an error, if you have any questions email uso@asf.alaska.edu\
+            \nError Text: HTTP {response.status_code}: {response.json()['errors']}")
         return
-    if r.status_code == 200:
+    if response.status_code == 200:
         logging.error("The asf-search module ecountered an error with CMR, and the following message was automatically reported to ASF:\n\n" + 
                         "\"\n" +
                         message + 
