779e2f2031f518094a9c51651fcf5e33af34672c
kim <jdfairbanks@alaska.edu>
2022-10-19 11:54:08 -0800

    adds burst properties cmr translation, burst product type constant

--- asf_search/CMR/translate.py
+++ asf_search/CMR/translate.py
@@ -201,6 +201,16 @@ def translate_product(item: dict) -> dict:
     else:
         properties['frameNumber'] = cast(int, get(umm, 'AdditionalAttributes', ('Name', 'CENTER_ESA_FRAME'), 'Values', 0))
 
+    if properties['processingLevel'] == 'BURST':
+        burst = {
+            'absoluteID': get(umm, 'AdditionalAttributes', ('Name', 'BURST_ID_ABSOLUTE'), 'Values', 0),
+            'relativeID': cast(int, get(umm, 'AdditionalAttributes', ('Name', 'BURST_ID_RELATIVE'), 'Values', 0)),
+            # 'timeFromAnxSec': cast(float, get(umm, 'AdditionalAttributes', ('Name', 'TIME_FROM_ANX_SEC'), 'Values', 0)),
+            'fullID': get(umm, 'AdditionalAttributes', ('Name', 'BURST_ID_FULL'), 'Values', 0),
+            'subswath': get(umm, 'AdditionalAttributes', ('Name', 'SUBSWATH_NAME'), 'Values', 0),
+        }
+        properties['burst'] = burst
+
     return {'geometry': geometry, 'properties': properties, 'type': 'Feature', 'baseline': baseline}
 
 def get_additional_fields(umm, *field_path):
