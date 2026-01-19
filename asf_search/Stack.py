9e6f47f824f3824afb3a8545de4676f728ab15f1
Alex-Lewandowski <aflewandowski@alaska.edu>
2026-01-19 12:41:25 -0900

    finish updating and generalizing Stack class, removing references to dates and SBAS-related baselines

--- asf_search/Stack.py
+++ asf_search/Stack.py
@@ -1,46 +1,39 @@
 from collections import defaultdict, deque
 from copy import copy
-from datetime import datetime, date
-from typing import Optional, List, Tuple, Dict
+from typing import Optional, List, Tuple
 import warnings
 
 from .ASFProduct import ASFProduct
 from .Pair import Pair
 from .ASFSearchOptions import ASFSearchOptions
 from .warnings import PairNotInFullStackWarning
-import numpy as np
-import pandas as pd
-
-try:
-    from ciso8601 import parse_datetime
-except ImportError:
-    from dateutil.parser import parse as parse_datetime
-
-date_like = str | date | datetime | pd.Timestamp
-
 
 class Stack:
     """
-    A Stack contains collections of Pairs
-
-    Stack objects contain several stacks as member variables, which are lists of Pair objects.
+    A Stack object contains 4+ lists of Pair objects. Each Pair contains a pair of asf_search.ASFProduct objects.
     
-    member stacks:
+    Stack member variables holding lists of Pairs:
     - self.full_stack: Every possible pair based on the provided geo_reference scene 
-    and ASFSearchOptions.
-    - self.subset_stack: The (possibly disconnected) SBAS stack
-    - self.connected_substacks: A list containing each disconnected component of self.subset_stack
+      and ASFSearchOptions. This forms a complete network of all represented asf_search.ASFProducts.
+    - self.remove_list: The list of Pairs to remove from self.full_stack, used to create self.subset_stack
+    - self.subset_stack: The resulting list after removing self.Remove_list from self.full_stack. This creates 
+      a possibly disconnected network of asf_search.ASFProducts
+    - self.connected_substacks: This is a list of lists of Pairs. It contains each disconnected component of a self.subset_stack's
+      asf_search.ASFProduct network. A length of 1 indicates that self.subset_stack is a connected network of asf_search.ASFProducts.
 
-    self._remove_list is a list of date pair tuples that is used for filtering a subset_stack from a full_stack
+    Public Stack methods:
+    - self.remove_pairs(): Adds Pairs to self.remove_list and removes them from self.subset_stack.
+    - self.add_pairs(): Adds Pairs to self.subset_stack. This either removes them from self.remove_list, or if not yet present in the
+      Stack, adds them to self.full_stack.
+    - self.get_scene_ids(): A convienience method that returns a given list of Pairs as a list of tuples of asf_search.ASFProduct
+      product IDs, which is useful when ordering on-demand processing via ASF's HyP3 or HyP3+ services.
     """
     def __init__(
         self,
         geo_reference: ASFProduct,
-        temporal_baseline: Optional[int] = None,
         opts: Optional[ASFSearchOptions] = None
     ):
         self.geo_reference = geo_reference
-        self.temporal_baseline = temporal_baseline
         if opts is None:
             opts = ASFSearchOptions()
         self.opts = opts
@@ -50,28 +43,25 @@ class Stack:
         self.connected_substacks = self._find_connected_substacks()
 
     @property
-    def remove_list(self) -> List[Tuple[date, date]]:
+    def remove_list(self) -> List[Pair]:
         """
-        Return a copy of self._remove_list so client changes 
-        do not alter self._remove_list without a stack update
+        Returns a copy of self._remove_list so client changes 
+        do not alter self._remove_list without initiating a stack update
 
         Disallow: 
           - my_stack.remove_list.append(my_pair)
           - my_stack.remove_list.remove(my_pair)
 
         Support:
-          - my_stack.remove_pairs([my_pair, some_other_pair, ...])
-          - my_stack.add_pairs([my_pair, some_other_pair, ...])
+          - my_stack.remove_pairs([pair_1, pair_2, ...])
+          - my_stack.add_pairs([pair_1, pair_2, ...])
         """
         return copy(self._remove_list)
 
     @remove_list.setter
     def remove_list(self, pairs: List[Pair]):
         """
-        Accept date pair tuples as datetime.datetime, datetime.date, 
-        pandas.Timestamp, or ISO datetime string.
-
-        Return tuples of datetime.date
+        pairs: A list of Pairs to remove from self.subset_stack
         """
         # remove duplicates
         self._remove_list = list(set(pairs))
@@ -81,6 +71,8 @@ class Stack:
         """
         Remove pairs from self.subset_stack, 
         i.e., add them to self._remove_list
+
+        pairs: A list of Pairs to remove from self.subset_stack
         """
         for pair in pairs:
             if pair not in self._remove_list:
@@ -94,11 +86,12 @@ class Stack:
     def add_pairs(self, pairs: List[Pair]):
         """
         Add pairs to self.subset_stack and, if necessary, to self.full_stack 
-        i.e., remove them from self._remove_list if present
-              or else add them to self.full_stack 
+        i.e., remove them from self._remove_list if present or else add them to self.full_stack 
 
         This allows for the addition of custom pairs that were not originally present
         in self.full_stack
+
+        pairs: A list of Pairs to add to self.subset_stack
         """
         for pair in pairs:
             if pair in self._remove_list:
@@ -107,36 +100,16 @@ class Stack:
                 self.full_stack.append(pair)
         self._update_stack()
 
-    def generate_pairs_within_baseline(self, dates):
-        dates = sorted(dates)
-        return [
-            (dates[i], dates[j])
-            for i in range(len(dates))
-            for j in range(i + 1, len(dates))
-            if (dates[j] - dates[i]).days <= self.temporal_baseline
-        ]
-
     def _build_full_stack(self) -> List[Pair]:
         """
         Create self._full_stack, which involves performing a stack search
-        of the georeference scene and creating a list of every possible Pair
-        in which both products contain StateVectors and have a temporal baseline
-        <=  self.temporal_baseline
+        of the georeference scene and creating a list of every possible Pair.
         """
         geo_ref_stack = self.geo_reference.stack(opts=self.opts)
         return [
             Pair(p1, p2)
             for i, p1 in enumerate(geo_ref_stack)
             for p2 in geo_ref_stack[i+1:]
-            if self.temporal_baseline is None
-            or 
-            (
-                (parse_datetime(p2.properties['stopTime']) - parse_datetime(p1.properties['stopTime'])).days <= self.temporal_baseline
-                and
-                p1.baseline.get("noStateVectors")
-                and
-                p2.baseline.get("noStateVectors")
-            )
         ]
 
     def _get_subset_stack(self) -> List[Pair]:
@@ -149,15 +122,15 @@ class Stack:
     def _update_stack(self):
         """
         Recalculate self.subset_stack and find its connected substacks.
-        These two things should always happen together
+        These two things should always happen together.
         """
         self.subset_stack = self._get_subset_stack()
         self.connected_substacks = self._find_connected_substacks()
 
 
-    def _find_connected_substacks(self) -> List[Dict[Tuple[date, date], Pair]]:
+    def _find_connected_substacks(self) -> List[List[Pair]]:
         """
-        BFS to find all connected components of self.subset_stack
+        Perform a bredth first search to find all connected components of self.subset_stack
         """
 
         graph = defaultdict(list)
@@ -199,21 +172,21 @@ class Stack:
 
         return components
 
-    def get_scene_ids(self, stack_list: Optional[List[Pair]] = None) -> List[Tuple[str, str]]:
+    def get_scene_ids(self, pair_list: Optional[List[Pair]] = None) -> List[Tuple[str, str]]:
         """
+        Provides scene names for all asf_search.ASFProducts in a list of Pairs.
         Useful when ordering an SBAS stack from ASF HyP3 On-Demand Processing.
-        Provides InSAR pair scene names in a list of tuples.
 
         If no stack_dict is passed, defaults to the largest connected substack
 
         Returns:
             A list tuples containing the reference and secondary scene name for each inSAR pair in the SBAS stack
         """
-        if not stack_list:
-            stack_list = max(self.connected_substacks, key=len)
+        if not pair_list:
+            pair_list = max(self.connected_substacks, key=len)
 
         return [
             (pair.ref.properties["sceneName"], pair.sec.properties["sceneName"])
-            for pair in stack_list
+            for pair in pair_list
             ]
     
