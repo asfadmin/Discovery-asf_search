000e9a2c8e660f2ace4cd926fa9c9bc801f70894
Alex-Lewandowski <aflewandowski@alaska.edu>
2026-06-03 13:01:53 -0800

    add multiburst SBASNetwork tests

--- tests/SBASNetwork/test_SBASNetwork.py
+++ tests/SBASNetwork/test_SBASNetwork.py
@@ -1,9 +1,14 @@
+import copy
 from datetime import date
 import pickle
 from pathlib import Path
 
-from asf_search.ASFSearchOptions import ASFSearchOptions
 from asf_search import SBASNetwork
+
+try:
+    from ciso8601 import parse_datetime
+except ImportError:
+    from dateutil.parser import parse as parse_datetime
 
 import pytest
 
@@ -40,16 +45,36 @@ def sbas_network(stack_results):
         inseason_temporal_baseline=24,
         bridge_target_date='3-1',
         bridge_year_threshold=2)
-    
-def test_sbas_network_from_ref_scene(mocker, reference, stack_results):
+
+@pytest.fixture(scope="module")
+def multiburst_dict():
+    return {
+        "173_370305": ("IW1", "IW2", "IW3"),
+        "173_370306": ("IW1", "IW2", "IW3"),
+        "173_370307": ("IW1", "IW2", "IW3"),
+    }
+
+@pytest.fixture(scope="module")
+def sbas_s1_multiburst_base(multiburst_dict):
+    return SBASNetwork.s1_multiburst(
+        multiburst_dict,
+        start_date='2023-01-01',
+        end_date='2025-10-02' ,
+        season=(1, 176),
+        perpendicular_baseline=100,
+        inseason_temporal_baseline=24,
+        bridge_target_date='3-1',
+        bridge_year_threshold=2,
+    )
+
+@pytest.fixture
+def sbas_s1_multiburst(sbas_s1_multiburst_base):
+    return copy.deepcopy(sbas_s1_multiburst_base)
+
+def test_sbas_network_from_ref_scene(reference):
     """
     Create an SBASNetwork from a geographic reference scene
     """
-    mock_stack = mocker.patch.object(
-        reference,
-        "stack",
-        return_value=stack_results,
-    )
 
     # Create an SBASNetwork and confrm expected size of its full_stack and subset_stack
     sbas = SBASNetwork(
@@ -135,3 +160,32 @@ def add_new_pair(sbas_network, pair):
     assert len(sbas_network.full_stack) == 1731
     assert len(sbas_network.subset_stack) == 176
     assert len(max(sbas_network.connected_substacks, key=lambda s: len(s))) == 164
+
+def test_check_s1_multiburst_group_validity_valid(multiburst_dict):
+    obj = SBASNetwork.__new__(SBASNetwork)
+    obj.geo_reference_multiburst_dict = multiburst_dict
+
+    obj.check_s1_multiburst_group_validiity()
+
+def test_multiburst_sbas(sbas_s1_multiburst):
+    assert len(sbas_s1_multiburst.s1_multiburst_sbas_networks) == 9
+    assert len(sbas_s1_multiburst.s1_multiburst_georeferences) == 9
+
+def test_add_pairs(sbas_s1_multiburst):
+    assert len(sbas_s1_multiburst.connected_substacks) == 2
+
+    geo_reference_dict_1 = {s.geo_reference: [
+    sbas_s1_multiburst.get_pair_from_dates(s.remove_list, parse_datetime("20230304").date(), parse_datetime("20240614").date()),
+    sbas_s1_multiburst.get_pair_from_dates(s.remove_list, parse_datetime("20230127").date(), parse_datetime("20230304").date()),
+    ] for s in sbas_s1_multiburst.s1_multiburst_sbas_networks}
+
+    sbas_s1_multiburst.s1_multiburst_add_pairs(geo_reference_dict_1)
+    assert len(sbas_s1_multiburst.connected_substacks) == 1
+
+def test_remove_pairs(sbas_s1_multiburst):
+    assert len(sbas_s1_multiburst.s1_multiburst_sbas_networks[0].subset_stack) == 84
+
+    pair = sbas_s1_multiburst.s1_multiburst_sbas_networks[0].subset_stack[0]
+    sbas_s1_multiburst.s1_multiburst_remove_pairs({'all': [pair]})
+
+    assert len(sbas_s1_multiburst.s1_multiburst_sbas_networks[0].subset_stack) == 83
