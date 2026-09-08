48dd7dcf1f74cda1a520b43ed2b34d7405a62c0f
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2026-09-08 11:26:06 -0800

    chore: re-enable old uavsar functionality for backwards compatability

--- asf_search/Products/UAVSARProduct.py
+++ asf_search/Products/UAVSARProduct.py
@@ -9,47 +9,42 @@ class UAVSARProduct(ASFProduct):
 
     _base_properties = {
         **ASFProduct._base_properties,
-        'groupID': {'path': ['AdditionalAttributes', ('Name', 'GROUP_ID'), 'Values', 0]},
-        'insarStackId': {'path': ['AdditionalAttributes', ('Name', 'INSAR_STACK_ID'), 'Values', 0]},
-        'processingLevel': {
-            'path': ['AdditionalAttributes', ('Name', 'PRODUCT_TYPE'), 'Values', 0]
+        "groupID": {"path": ["AdditionalAttributes", ("Name", "GROUP_ID"), "Values", 0]},
+        "insarStackId": {"path": ["AdditionalAttributes", ("Name", "INSAR_STACK_ID"), "Values", 0]},
+        "md5sum": {"path": ["AdditionalAttributes", ("Name", "MD5SUM"), "Values", 0]},
+        "processingLevel": {
+            "path": ["AdditionalAttributes", ("Name", "PRODUCT_TYPE"), "Values", 0]
         },
-        'polarization': {
-            'path': ['AdditionalAttributes', ('Name', 'POLARIZATION'), 'Values']
+        "polarization": {
+            "path": ["AdditionalAttributes", ("Name", "POLARIZATION"), "Values"]
         },  # for consolidated collection
-        'bytes': {'path': ['DataGranule', 'ArchiveAndDistributionInformation']},
+        "bytes": {"path": ["DataGranule", "ArchiveAndDistributionInformation"]},
     }
 
     def __init__(self, args: Dict = {}, session: ASFSession = ASFSession()):
         super().__init__(args, session)
 
-        bytes_mapping = {
-            entry['Name']: {'bytes': entry['SizeInBytes'], 'format': entry['Format']}
-            for entry in self.properties['bytes']
-        }
-        md5sum_mapping = {
-            entry['Name']: entry['Checksum']['Value'] for entry in self.properties['bytes']
-        }
+        if self.properties["bytes"] is None:
+            self.properties["bytes"], self.properties["md5sum"] = self._get_file_sizes_and_sums()
 
-        self.properties['bytes'] = bytes_mapping
-        self.properties['md5sum'] = md5sum_mapping
+            self.properties["additionalUrls"] = [
+                url for url in self._get_additional_urls() if not url.endswith("-END")
+            ]
+            self.properties["browse"] = [
+                url
+                for url in self._get_urls()
+                if url.endswith(".png")
+                or url.endswith(".jpg")
+                or url.endswith(".jpeg")
+                or url.endswith(".gif")
+            ]
 
-        self.properties['additionalUrls'] = [
-            url for url in self._get_additional_urls() if not url.endswith('-END')
-        ]
+            self.properties["s3Urls"] = self._get_s3_uris()
 
-        self.properties['browse'] = [
-            url
-            for url in self._get_urls()
-            if url.endswith('.png')
-            or url.endswith('.jpg')
-            or url.endswith('.jpeg')
-            or url.endswith('.gif')
-        ]
-        self.properties['s3Urls'] = self._get_s3_uris()
+            self.properties["conceptID"] = self.umm_get(self.meta, "collection-concept-id")
 
-        center = self.centroid()
-        self.properties['centerLat'] = center.y
-        self.properties['centerLon'] = center.x
+            center = self.centroid()
+            self.properties["centerLat"] = center.y
+            self.properties["centerLon"] = center.x
 
-        self.properties['platform'] = 'UAVSAR'
+            self.properties["platform"] = "UAVSAR"
