7313ff98f7808684f8b5520a0825678605aebf21
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2026-09-09 08:36:01 -0800

    bug: pass FileSizeKeys to _get_file_sizes_and_sums()

--- asf_search/ASFProduct.py
+++ asf_search/ASFProduct.py
@@ -466,7 +466,9 @@ class ASFProduct:
 
     def _set_additional_metadata(self):
         """Helper method for data migrated off-prem"""
-        self.properties["bytes"], self.properties["md5sum"] = self._get_file_sizes_and_sums()
+        self.properties["bytes"], self.properties["md5sum"] = self._get_file_sizes_and_sums(
+            FileSizeKeys()
+        )
         self.properties["additionalUrls"] = self._get_additional_urls()
         self.properties["browse"] = [
             url for url in self._get_urls() if url.endswith(self._default_browse_extensions)
